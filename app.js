6252183ff8b60b2a6214be72b32784190636cbf2
govea <ctgovea@gmail.com>
2018-12-11 18:24:59 -0600

    routing

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const bookRouter = express.Router();
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -14,6 +16,18 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views/');
 app.set('view engine', 'ejs');
+
+bookRouter.route('/')
+  .get((req, res) => {
+    res.send('Hello books');
+  });
+
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('hello single book');
+  });
+
+app.use('/books', bookRouter);
 
 app.get('/', (req, res) => {
   res.render(
