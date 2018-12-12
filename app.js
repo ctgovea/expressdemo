13edadb5ee96681a2fb90b56e6b76b455ee3c01d
govea <ctgovea@gmail.com>
2018-12-11 18:18:07 -0600

    add navigation

--- app.js
+++ app.js
@@ -16,7 +16,13 @@ app.set('views', './src/views/');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Library', list: ['a', 'b'] });
+  res.render(
+    'index',
+    {
+      nav: [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }],
+      title: 'Library'
+    }
+  );
 });
 
 app.listen(port, () => {
