c06a8e458405c4927760339243cd5831fe276d24
govea <ctgovea@gmail.com>
2018-12-18 14:46:52 -0600

    Local strategy

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny')); // looks at requests and prints to the console.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ secret: 'library' }));
 
-// app.use((req, res, next) => {
-//   debug('my middleware');
-//   next();
-// });
+require('./src/config/passport.js')(app);
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -21,14 +26,20 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views/');
 app.set('view engine', 'ejs');
 
+// app.use((req, res, next) => {
+//   debug('my middleware');
+//   next();
+// });
+
 const nav = [{ link: '/books', title: 'Book' }, { link: '/authors', title: 'Author' }];
 
 const bookRouter = require('./src/routes/bookRoutes')(nav);
 const adminRouter = require('./src/routes/adminRoutes')(nav);
-
+const authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use('/books', bookRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
   res.render(
